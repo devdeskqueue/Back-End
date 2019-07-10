8416a75bd57d8fc05901187324f28579d010f972
Louis Magdaleno <buildthatapp@gmail.com>
2019-07-10 15:56:38 -0700

    Added initial test

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const server = require("./server");
+const express = require("express");
 
-// local variables
+const app = express();
+
+app.use(express.json());
+
 const PORT = 3000;
 
-server.listen(PORT, () => {
+// Respond with "hello world" for requests that hit our root "/"
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
 });
+
+module.exports = app;
