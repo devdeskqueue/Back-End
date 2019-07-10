ac2d39b23049cc15da3d9556b6efca428e2a4e95
Louis Magdaleno <buildthatapp@gmail.com>
2019-07-10 15:46:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const server = require("./server");
 // local variables
 const PORT = 3000;
 
+//Respond with "hello world" for requests that hit our root "/"
+server.get(‘/’, function (req, res) {
+ res.send(‘hello world’);
+});
+
 server.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
 });
