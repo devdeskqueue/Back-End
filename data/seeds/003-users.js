2920d35eeea4872574c5784ff020c5f2bc15c578
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-07-12 16:49:16 -0700

    Fix column name format for consistency

--- data/seeds/003-users.js
+++ data/seeds/003-users.js
@@ -5,24 +5,24 @@ exports.seed = async function(knex) {
   await knex('Users').insert([
     { 
       id: 1,
-      firstName: 'George',
-      lastName: 'Kaplan',
+      first_name: 'George',
+      last_name: 'Kaplan',
       email: 'gkaplan@mail.com',
       password: bcrypt.hashSync('pass', 12),
       role_id: 1
     },
     { 
       id: 2,
-      firstName: 'Cindy',
-      lastName: 'Brady',
+      first_name: 'Cindy',
+      last_name: 'Brady',
       email: 'cindyb@gmail.com',
       password: bcrypt.hashSync('pass', 12),
       role_id: 2
     },
     { 
       id: 3,
-      firstName: 'Steve',
-      lastName: 'Rogers',
+      first_name: 'Steve',
+      last_name: 'Rogers',
       email: 'steve@hotmail.com',
       password: bcrypt.hashSync('pass123', 12),
       role_id: 2
