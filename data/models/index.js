096509c51eeed6ed4d0596d71277b4a252650844
Louis Magdaleno <buildthatapp@gmail.com>
2019-07-12 15:46:37 -0700

    Updated db methods

--- data/models/index.js
+++ data/models/index.js
@@ -1,48 +1,88 @@
-let db = [];
+const db = require("../dbConfig");
 
-async function getUsers() {
+async function findByTable(table, ticketID = null) {
   try {
-    return db;
-  } catch (err) {
-    return err;
-  }
-}
+    let data;
+    if (ticketID) {
+      data = await db(table)
+        .where(ticketID, "id")
+        .orderBy("id");
+    } else {
+      data = await db(table).orderBy("id");
+    }
 
-function findById(id) {
-  try {
-    let data = db.filter(user => user.id === id)[0];
     return data;
   } catch (err) {
     return err;
   }
 }
 
-function findByUser(username) {
+async function findById(id, table) {
   try {
-    let data = db.filter(user => user.username === username)[0];
+    let data = await db(table)
+      .select("id", "username")
+      .where({ id: Number(id) })
+      .first();
     return data;
   } catch (err) {
     return err;
   }
 }
 
-function insert(data) {
+async function findByUser(username, table) {
   try {
-    db.push({ ...data, id: db.length });
-    let newRecordId = db[db.length - 1].id;
-    let newRecord = findById(newRecordId);
+    let data = await db(table)
+      .where({ username })
+      .first();
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
+
+async function findByField(table, field, data) {
+  try {
+    let query = await db(table)
+      .where(`${field}`, data)
+      .first();
+    return query;
+  } catch (err) {
+    return err;
+  }
+}
+
+async function insert(data, table) {
+  try {
+    let newRecordId = await db(table).insert(data, "id");
+    let newRecord = await findById(newRecordId, table);
     return newRecord;
   } catch (err) {
     return err;
   }
 }
 
-function remove(id) {
+async function update(id, data, table) {
   try {
-    let user = db.find(user => user.id === id);
-    db = db.filter(user => user.id !== id);
+    let updateCount = await db(table)
+      .where({ id })
+      .update(data);
+    if (updateCount > 0) {
+      let updatedRecord = await findById(id, table);
+      return updatedRecord;
+    }
+  } catch (err) {
+    return err;
+  }
+}
 
-    return "success";
+async function remove(id, table) {
+  try {
+    let deleteCount = await db(table)
+      .where({ id })
+      .del();
+    if (deleteCount > 0) {
+      return "delete successful";
+    }
   } catch (err) {
     return err;
   }
@@ -50,9 +90,11 @@ function remove(id) {
 
 module.exports = {
   db,
+  findByTable,
   findById,
   findByUser,
+  findByField,
   insert,
-  remove,
-  getUsers
+  update,
+  remove
 };
