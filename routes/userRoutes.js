a3216f0e31df3177259b66d20010ef0a8c214e8b
Louis Magdaleno <buildthatapp@gmail.com>
2019-07-12 16:47:24 -0700

    Added morgan for logging

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -24,7 +24,7 @@ async function register(req, res) {
   const hash = bcrypt.hashSync(user.password, 14);
   user.password = hash;
   try {
-    let newUser = await db.insert(user, "users");
+    let newUser = await db.insert(user, "Users");
     let token = generateToken(newUser);
     res.status(201).json({
       message: `Welcome ${newUser.email}`,
@@ -38,7 +38,7 @@ async function register(req, res) {
 async function login(req, res) {
   let { email, password } = req.body;
   try {
-    let user = await db.findByUser(email, "users");
+    let user = await db.findByUser(email, "Users");
     if (user && bcrypt.compareSync(password, user.password)) {
       let token = generateToken(user);
       res.json({
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const logger = require("morgan");
 
 const userRoutes = require("./routes/userRoutes");
 
@@ -9,12 +10,13 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-
-// Respond with "hello world" for requests that hit our root "/"
-server.get("/", (req, res) => {
-  res.send("hello world");
-});
+server.use(logger("dev"));
 
 userRoutes(server);
+
+// Respond with "hello world" for requests that hit our root "/"
+server.use("/", (req, res) => {
+  res.send("hello world");
+});
 
 module.exports = server;
