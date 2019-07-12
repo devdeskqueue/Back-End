3d2f7a23df5e0954708927bb3f59a5d9f3de3183
Louis Magdaleno <buildthatapp@gmail.com>
2019-07-12 11:35:16 -0700

    register and login working

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,49 +1,66 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const db = require("knex")(require("../knexfile").development);
-const jwtKey = process.env.JWT_KEY;
+
+const db = require("../database/models");
+
+const { inputDataChecker, requiredData } = require("../auth/authenticate");
+const requiredFields = ["username", "password"];
 
 module.exports = server => {
-  server.post("/api/login", login);
-  server.post("/api/register", register);
+  server.post(
+    "/api/register",
+    requiredData(inputDataChecker, requiredFields),
+    register
+  );
+  server.post(
+    "/api/login",
+    requiredData(inputDataChecker, requiredFields),
+    login
+  );
 };
 
+async function register(req, res) {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+  try {
+    let newUser = await db.insert(user, "users");
+    let token = generateToken(newUser);
+    res.status(201).json({
+      message: `Welcome ${newUser.username}`,
+      token
+    });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+async function login(req, res) {
+  let { username, password } = req.body;
+  try {
+    let user = await db.findByUser(username, "users");
+    if (user && bcrypt.compareSync(password, user.password)) {
+      let token = generateToken(user);
+      res.json({
+        message: `Welcome ${user.username}`,
+        token
+      });
+    } else {
+      res.status(401).json({ message: "Incorrect username or password" });
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
 function generateToken(user) {
-  const payload = {
-    userId: user.id,
-    username: user.username
-  };
-  const secret = jwtKey;
-  const options = {
-    expiresIn: "1h"
-  };
-  return jwt.sign(payload, secret, options);
-}
-
-function login(req, res) {
-  const creds = req.body;
-  db("users")
-    .where({ username: creds.username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        const token = generateToken(user);
-        res.status(200).json({ message: "Welcome!", token });
-      } else {
-        res.status(401).json({ message: "you shall not pass!!" });
-      }
-    })
-    .catch(err => res.status(400).json(err));
-}
-
-function register(req, res) {
-  const creds = req.body;
-  const hash = bcrypt.hashSync(creds.password, 10);
-  creds.password = hash;
-  db("users")
-    .insert(creds)
-    .then(ids => {
-      res.status(201).json(ids);
-    })
-    .catch(err => res.status(400).json(err));
+  return jwt.sign(
+    {
+      userId: user.id
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "2h"
+    }
+  );
 }
