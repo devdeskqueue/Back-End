86fb3f89a708927dd44016b02ee92a5b6ad0e3d0
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-07-12 14:57:50 -0700

    Remove username field

--- data/migrations/20190711223943_users.js
+++ data/migrations/20190711223943_users.js
@@ -2,14 +2,15 @@
 exports.up = async function(knex) {
   await knex.schema.createTable('Users', tbl => {
     tbl.increments('id')
-    tbl.string('username', 255)
     tbl.string('firstName', 128)
     tbl.string('lastName', 128)
-    tbl.string('password', 128)
-    tbl.string('email', 128)
+    tbl.string('password', 128).notNullable()
+    tbl.string('email', 128).notNullable()
     tbl.integer('role_id')
+      .notNullable()
+      .defaultTo(2)
       .references('id')
-      .inTable('Users')
+      .inTable('Roles')
   })
 };
 
