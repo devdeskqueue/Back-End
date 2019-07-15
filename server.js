0eca72018b7adf20d10ce78fdf6a3ff464b78c31
Louis Magdaleno <buildthatapp@gmail.com>
2019-07-15 10:23:08 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const logger = require("morgan");
 
 const userRoutes = require("./routes/userRoutes");
+const ticketRoutes = require("./routes/ticketRoutes")
+const commentRoutes = require("./routes/commentRoutes")
+const categoriesRoutes = require("./routes/categoriesRoutes")
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-server.use(logger("dev"));
-
-userRoutes(server);
 
 // Respond with "hello world" for requests that hit our root "/"
-server.use("/", (req, res) => {
+server.get("/", (req, res) => {
   res.send("hello world");
 });
+
+// Route handling
+userRoutes(server);
+server.use('/api/tickets', ticketRoutes)
+server.use('/api/tickets/:ticket_id/comments', commentRoutes)
+server.use('/api/categories', categoriesRoutes)
 
 module.exports = server;
