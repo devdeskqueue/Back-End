b4c10069b7df81a544cef81d30fe6eb85fbfe46c
Louis Magdaleno <buildthatapp@gmail.com>
2019-07-15 16:27:14 -0700

    fixed typos in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const bcrypt = require("bcryptjs");
-const db = require("../dbConfig");
+const db = require("./data//dbConfig");
 const { isValidEmail, isValidPassword } = require("./middleware/middleware");
 const jwt = require("jsonwebtoken");
 
@@ -19,27 +19,20 @@ server.use(express.json(), helmet(), cors());
 const secret = process.env.JWT_SECRET_KEY;
 
 const generateToken = user => {
-  const { Firstname, Lastname, username, admin, avatar, id } = user;
+  const { first_name, last_name, email, id } = user;
   const payload = {
-    Firstname,
-    Lastname,
-    username,
-    admin,
-    avatar,
+    first_name,
+    last_name,
+    email,
     id
   };
   const options = {
     expiresIn: "1h",
-    jwtid: bcrypt.hashSync(user.username, 4),
+    jwtid: bcrypt.hashSync(user.email, 4),
     subject: `${id}`
   };
   return jwt.sign(payload, secret, options);
 };
-
-// Respond with "hello world" for requests that hit our root "/"
-server.get("/", (req, res) => {
-  res.send("hello world");
-});
 
 // Route handling
 server.use("/api/tickets", ticketRoutes);
@@ -51,18 +44,17 @@ server.post("/register", isValidPassword, isValidEmail, (req, res) => {
   const creds = req.body;
   if (
     !creds.password ||
-    !creds.username ||
-    !creds.Firstname ||
-    !creds.Lastname ||
+    !creds.first_name ||
+    !creds.last_name ||
     !creds.email
   ) {
     res.status(400).json({ error: "All fields are required!" });
   } else {
     creds.password = bcrypt.hashSync(creds.password, 12);
-    db("users")
+    db("Users")
       .insert(creds)
       .then(ids => {
-        db("users")
+        db("Users")
           .where("id", ids[0])
           .first()
           .then(user => res.status(201).json(generateToken(user)));
@@ -74,17 +66,17 @@ server.post("/register", isValidPassword, isValidEmail, (req, res) => {
 server.post("/login", (req, res) => {
   const creds = req.body;
   if (!creds.username || !creds.password) {
-    res
-      .status(400)
-      .json({ message: "Username and Password are both required!" });
+    res.status(400).json({ message: "Email and Password are both required!" });
   } else {
-    db("users")
-      .where("username", creds.username)
+    db("Users")
+      .where("email", creds.email)
       .first()
       .then(user =>
         user && bcrypt.compareSync(creds.password, user.password)
           ? res.status(200).json(generateToken(user))
-          : res.status(401).json({ message: "Invalid username or password!" })
+          : res
+              .status(401)
+              .json({ message: "Invalid usernaemailme or password!" })
       )
       .catch(err => res.status(500).json(err));
   }
